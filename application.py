af15064a91eb9ace2bf090d9ffe092cd47c1886a
varshha <varshhaks@example.com>
2020-07-19 17:00:05 +0530

    allbuttons

--- application.py
+++ application.py
@@ -15,29 +15,68 @@ data= {
                 9:[2,2]
             }
 reversed_data = {tuple(value) : key for (key, value) in data.items()}
+global i
+i=0
 
 app=Flask(__name__)
 @app.route('/')
 def json():
-    global board
-    board=index.Board(1)
+    global start
+    start=0
     return render_template('firstattempt.html')
 
-#background process happening without any refreshing
-@app.route('/jstoflask',methods=["POST"])
-def jstoflask():
+@app.route('/ai',methods=["POST"])
+def ai():
+    global game,difficulty,opponent,board,start
+    if opponent=="friend":
+        return "friend"
+    
+    if(start!=1):
+        return jsonify("Choose the settings first and start a new game to start playing!")
     id=int(request.form["1"])
     position=data[id]
-    if(position in board.available_positions_function()):
+    if(position in board.available_positions_function() and not (board.isWinner())):
        board.update_position(position,'o')
        bestmove=bestmoves(board.board_function(),board.depth_function())
-       id=reversed_data[tuple(bestmove)]
-       board.update_position(bestmove,'x')
-       winner=eval(board.board_function())
-       return jsonify([id,winner])
+       bestmove_id=reversed_data[tuple(bestmove)]
+       board.update_position(bestmove,'x')       
+       return jsonify(bestmove_id)
     else:
         print("position not available")
         return jsonify(0)
+@app.route('/setting',methods= ['POST'])
+def setting():
+  global game,difficulty,opponent,board,start
+  game= request.form['game']
+  difficulty= request.form['difficulty']
+  opponent= request.form['opponent']
+  board=index.Board(difficulty)
+  start=1
+  return jsonify(opponent)
+@app.route('/human',methods= ['POST'])
+def human():
+    global i
+    id=int(request.form["1"])
+    position=data[id]
+    if(position in board.available_positions_function() and not (board.isWinner())):
+         if(i%2==0):
+             board.update_position(position,'x')
+             i+=1
+             return jsonify("player1")
+             
+       
+         else:
+             board.update_position(position,'o')
+             i+=1
+             return jsonify("player2")
+            
+    else:
+        return jsonify(0)
+   
+
+
+    
+    
 
 if __name__=="__main__":
    app.run(debug=True)
--- index.py
+++ index.py
@@ -1,19 +1,28 @@
-import numpy as np
+from minimax import eval
 class Board:
     def __init__(self,maxDepth):
         self.board= [[ '', '', '' ], [ '', '', '' ],  [ '', '', '' ]]
         self.available_positions=[[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
         self.maxDepth=maxDepth
+        self.winner= None
+        self.orientation= None
+        self.orientation_no= None
     def update_position(self,position,player_symbol):
         self.board[position[0]][position[1]]=player_symbol
-        print("_____________")
-        print(position)
-        print("1")
         if(self.available_positions!=[]):
            self.available_positions.remove(position)
-        print("2")
-        print(position)
-        print("_____________")
+        self.winner_function()
+    def winner_function(self):
+        result= eval(self.board)
+        self.winner=result[0]
+        self.orientation=result[2]
+        self.orientation_no=result[1]
+        return result
+    def isWinner(self):
+        if self.winner==10 or self.winner==-10:
+            return True
+        else:
+            return False
     def available_positions_function(self):
         return self.available_positions
     def board_function(self):
